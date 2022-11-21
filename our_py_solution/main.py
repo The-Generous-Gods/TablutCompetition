bfd089394e8a30ce927b56070313604ca1c57129
calin <diaconuccalin@gmail.com>
2022-11-21 21:54:35 +0100

    add new monte carlo and run with arguments

--- our_py_solution/main.py
+++ our_py_solution/main.py
@@ -1,12 +1,23 @@
 from tablut_game import TablutGame
-from given_resources.games4e import monte_carlo_tree_search
+from given_resources.games4e import our_monte_carlo
+import numpy as np
+import sys
 
 
-def compute_move(state, game, max_steps):
-    # TODO: modify in such a way as to save the tree from one step to another
-    # TODO: modify in such a way as to avoid repetitive nodes
-    # TODO: modify instead of max_steps give max_time
-    return monte_carlo_tree_search(state, game, max_steps)
+def get_move_from_state_diff(old_board, new_board):
+    changes = np.argwhere(old_board != new_board)
+
+    if old_board[changes[0]] != 0:
+        return tuple(changes[0]), tuple(changes[1])
+    else:
+        return tuple(changes[1]), tuple(changes[0])
+
+
+def compute_move(state, game, max_steps, tree):
+    # TODO: maybe modify instead of max_steps give max_time or keep max_steps if constant ish
+    tree = our_monte_carlo(state, game, max_steps, tree)
+    move = get_move_from_state_diff(state.board, tree.state.board)
+    return move, tree
 
 
 def send(move):
@@ -19,24 +30,34 @@ def wait():
     return "WAITING"
 
 
-if __name__ == '__main__':
-    # TODO: Bash initialization
-    role = "w"
-        # TODO: Method to get role from given arguments
-        # TODO: Connect with json
-        # TODO: Other stuff from bash
-
+def the_main(role, time_out, address):
+    # TODO: Connect with json
     # Initialize game
     the_game = TablutGame(role)
     current_state = the_game.initial
+    tree = None
 
     while True:
         if current_state.to_move == role:
-            my_move = compute_move(current_state, the_game, 1000)
+            my_move, tree = compute_move(current_state, the_game, 1000, tree)
             current_state = the_game.result(current_state, my_move)
             send(my_move)
         else:
             their_move = wait()
             current_state = the_game.result(current_state, their_move)
+            tree = tree.children.get(current_state)
 
     # TODO: Check OneNote and rules once again to check that it's all
+
+
+if __name__ == '__main__':
+    role = sys.argv[1]
+    time_out = sys.argv[2]
+    address = sys.argv[3]
+
+    if "w" in role.lower():
+        role = "w"
+    else:
+        role = "b"
+
+    # the_main(role, time_out, address)
---
+++ our_py_solution/runmyplayer.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# java -jar /home/tablut/tablut/RandomPlayer.jar "$1" "$2" "$3"
+python main.py "$1" "$2" "$3"
