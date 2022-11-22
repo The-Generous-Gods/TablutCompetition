95558970f7552c7df6f88dc67c9e5e2ca6423ee9
calin <diaconuccalin@gmail.com>
2022-11-22 20:16:27 +0100

    Fix to temp connect script

--- our_py_solution/simple_client_trial.py
+++ our_py_solution/simple_client_trial.py
@@ -1,10 +1,24 @@
+# White port 5800
+# Black port 5801
+
 import requests
 import sys
+import socket
 
 url = sys.argv[1]
+port = sys.argv[2]
+
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.connect((url, port))
+
+"""
 data = {
     "data": "test_data"
 }
 
 r = requests.post(url, json=data)
 print(r.status_code)
+"""
+
+while True:
+    print(client_socket.accept())
